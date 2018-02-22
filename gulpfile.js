71e0a04e5e37a113078ad6ed258618fbf046d127
Leon Hassan <leonhassan@hotmail.com>
2018-02-22 15:06:07 +0000

    added gulp tasks and npm dependencies

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let rename = require('gulp-rename');
+let concat = require('gulp-concat');
+let minify = require('gulp-minify-css');
+let browserSync = require('browser-sync').create();
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+  browserSync.init({
+    server: "./root"
+  });
+  browserSync.stream();
+
+  gulp.watch('./root/scss/**/*.scss', ['sass']);
+  gulp.watch('./root/css/style.css', ['css']);
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./root/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./root/css'));
+});
+
+gulp.task('css', function(){
+   gulp.src('./root/css/style.css')
+   .pipe(concat('style.css'))
+   .pipe(minify())
+   .pipe(rename('style.css'))
+   .pipe(gulp.dest('./dist/css/'))
+   .pipe(browserSync.stream());
+});
